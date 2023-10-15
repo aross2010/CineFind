0218d5eff6a2292f15c27065c26657f9e2df1786
Alex Ross <adross1027@gmail.com>
2023-10-15 14:32:54 -0700

    body parser

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
@@ -15,9 +16,9 @@ const likeRoutes = require('./routes/likeRoutes')
 const userRoutes = require('./routes/userRoutes')
 const gameRoutes = require('./routes/gameRoutes')
 // middleware
-app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(express.urlencoded({ extended: false }))
 app.use(
   cors({
     credentials: true,
