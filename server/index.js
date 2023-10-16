7e39cc95adee55cf342a00de93638c48613517f8
Alex Ross <adross1027@gmail.com>
2023-10-16 14:06:40 -0700

    #

--- server/index.js
+++ server/index.js
@@ -25,7 +25,6 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
     'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Origin': true,
   })
   next()
 })
