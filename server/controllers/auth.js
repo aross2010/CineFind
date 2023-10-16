c3d678ec11489fb2c2cc4cd65523fe1e96270c65
Alex Ross <adross1027@gmail.com>
2023-10-15 23:40:48 -0700

    #

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -101,7 +101,9 @@ const loginUser = async (req, res) => {
           console.log(err)
           throw err
         }
-        res.cookie('token', token).json({ username: user.name })
+        res
+          .cookie('token', token)
+          .json({ username: user.name, path: '/auth/login' })
       }
     )
   } catch (e) {
