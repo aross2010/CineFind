c4d0e312cce7ec582f1417e8b122ee8549e771a1
Alex Ross <adross1027@gmail.com>
2023-10-15 20:22:40 -0700

    #

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import Popup from './compenents/Popup'
 import Loading from './compenents/LoadingSpinner'
 import { UserContext } from './context/userContext'
 import useWindowSizeHook from './hooks/widowSizeHook'
-import Styles from './compenents/Styles'
 
 export const AppContext = React.createContext()
 
@@ -22,7 +21,14 @@ function App() {
 
   return (
     <>
-      <Styles />
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+          crossorigin="anonymous"
+        />
+      </head>
       <div style={{ minHeight: '100vh' }}>
         <Navbar />
         <div
