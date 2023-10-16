bf9a01780a4229cd72f934825b81698b0b9be1ba
Alex Ross <adross1027@gmail.com>
2023-10-15 20:06:46 -0700

    #

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,14 @@ function App() {
 
   return (
     <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+          crossorigin="anonymous"
+        />
+      </Head>
       <div style={{ minHeight: '100vh' }}>
         <Navbar />
         <div
