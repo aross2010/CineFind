a31b6e163cdc26b720cb2637114e24d80fc49024
Alex Ross <adross1027@gmail.com>
2023-10-15 20:46:39 -0700

    #

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Popup from './compenents/Popup'
 import Loading from './compenents/LoadingSpinner'
 import { UserContext } from './context/userContext'
 import useWindowSizeHook from './hooks/widowSizeHook'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/style.css'
 import './styles/cast.css'
 import './styles/checkbox.css'
@@ -40,14 +41,7 @@ function App() {
 
   return (
     <>
-      <head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-          crossorigin="anonymous"
-        />
-      </head>
+      <head></head>
       <div style={{ minHeight: '100vh' }}>
         <Navbar />
         <div
