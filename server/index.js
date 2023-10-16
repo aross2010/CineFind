5281a23306979d44071ff796ccab3f72aee57e80
Alex Ross <adross1027@gmail.com>
2023-10-16 14:11:01 -0700

    $

--- server/index.js
+++ server/index.js
@@ -21,7 +21,6 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use((req, res, next) => {
   res.set({
-    'Access-Control-Allow-Origin': 'https://cinefind.vercel.app',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
     'Access-Control-Allow-Credentials': true,
