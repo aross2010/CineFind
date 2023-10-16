864c7812a657fc2249274e77767a682baef8f689
Alex Ross <adross1027@gmail.com>
2023-10-16 16:11:07 -0700

    #

--- server/index.js
+++ server/index.js
@@ -20,7 +20,13 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin')
+  res.setHeader('Access-Control-Allow-Origin: *')
+  res.setHeader(
+    'Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'
+  )
   next()
 })
 app.use(
